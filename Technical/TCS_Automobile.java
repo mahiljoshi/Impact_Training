879a3ef9aeaaab1e2fc4f91f06a3ef9cee7c2e79
Mahil Joshi <mahil.joshi2006@gmail.com>
2026-05-09 12:58:55 +0530

    Update TCS_Automobile.java

--- Technical/TCS_Automobile.java
+++ Technical/TCS_Automobile.java
@@ -1,4 +1,4 @@
-/*Problem Statement – An automobile company manufactures both a two wheeler (TW) and a four wheeler (FW). A company manager wants to make the production of both types of vehicle according to the given data below:
+/****Problem Statement – An automobile company manufactures both a two wheeler (TW) and a four wheeler (FW). A company manager wants to make the production of both types of vehicle according to the given data below:
 
 1st data, Total number of vehicle (two-wheeler + four-wheeler)=v
 2nd data, Total number of wheels = W
